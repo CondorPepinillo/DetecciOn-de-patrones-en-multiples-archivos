08eed04b134d09b461fa628f701470f326074f67
CondorPepinillo <marttapia2023@udec.cl>
2025-07-01 13:21:45 -0400

    Implementacion de Rabin-Karp y el que los distintos algoritmos esten en clases y sus objetos

--- boyer_moore.cpp
+++ boyer_moore.cpp
@@ -9,79 +9,81 @@ Moore String Matching Algorithm */
 #include <sstream>
 using namespace std;
 #define NO_OF_CHARS 256
-
-// The preprocessing function for Boyer Moore's
-// bad character heuristic
-void badCharHeuristic(string str, int size,
-                      int badchar[NO_OF_CHARS])
+class BoyerMoore
 {
-    int i;
+public:
+    // The preprocessing function for Boyer Moore's
+    // bad character heuristic
+    void badCharHeuristic(string str, int size,
+                        int badchar[NO_OF_CHARS])
+    {
+        int i;
 
-    // Initialize all occurrences as -1
-    for (i = 0; i < NO_OF_CHARS; i++)
-        badchar[i] = -1;
+        // Initialize all occurrences as -1
+        for (i = 0; i < NO_OF_CHARS; i++)
+            badchar[i] = -1;
 
-    // Fill the actual value of last occurrence
-    // of a character
-    for (i = 0; i < size; i++)
-        badchar[(int)str[i]] = i;
-}
-
-/* A pattern searching function that uses Bad
-Character Heuristic of Boyer Moore Algorithm */
-void search(string txt, string pat, int* count)
-{
-    int m = pat.size();
-    int n = txt.size();
-
-    int badchar[NO_OF_CHARS];
-
-    /* Fill the bad character array by calling
-    the preprocessing function badCharHeuristic()
-    for given pattern */
-    badCharHeuristic(pat, m, badchar);
-
-    int s = 0; // s is shift of the pattern with
-               // respect to text
-    while (s <= (n - m)) {
-        int j = m - 1;
-
-        /* Keep reducing index j of pattern while
-        characters of pattern and text are
-        matching at this shift s */
-        while (j >= 0 && pat[j] == txt[s + j])
-            j--;
-
-        /* If the pattern is present at current
-        shift, then index j will become -1 after
-        the above loop */
-        if (j < 0) {
-            //cout << "pattern occurs at shift = " << s
-                // << endl;
-            (*count)++; // Increment the count of occurrences
-            
-            /* Shift the pattern so that the next
-            character in text aligns with the last
-            occurrence of it in pattern.
-            The condition s+m < n is necessary for
-            the case when pattern occurs at the end
-            of text */
-            s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
-        }
-
-        else
-            /* Shift the pattern so that the bad character
-            in text aligns with the last occurrence of
-            it in pattern. The max function is used to
-            make sure that we get a positive shift.
-            We may get a negative shift if the last
-            occurrence of bad character in pattern
-            is on the right side of the current
-            character. */
-            s += max(1, j - badchar[txt[s + j]]);
+        // Fill the actual value of last occurrence
+        // of a character
+        for (i = 0; i < size; i++)
+            badchar[(int)str[i]] = i;
     }
-}
 
+    /* A pattern searching function that uses Bad
+    Character Heuristic of Boyer Moore Algorithm */
+    void search(string txt, string pat, int* count)
+    {
+        int m = pat.size();
+        int n = txt.size();
+
+        int badchar[NO_OF_CHARS];
+
+        /* Fill the bad character array by calling
+        the preprocessing function badCharHeuristic()
+        for given pattern */
+        badCharHeuristic(pat, m, badchar);
+
+        int s = 0; // s is shift of the pattern with
+                // respect to text
+        while (s <= (n - m)) {
+            int j = m - 1;
+
+            /* Keep reducing index j of pattern while
+            characters of pattern and text are
+            matching at this shift s */
+            while (j >= 0 && pat[j] == txt[s + j])
+                j--;
+
+            /* If the pattern is present at current
+            shift, then index j will become -1 after
+            the above loop */
+            if (j < 0) {
+                //cout << "pattern occurs at shift = " << s
+                    // << endl;
+                (*count)++; // Increment the count of occurrences
+                
+                /* Shift the pattern so that the next
+                character in text aligns with the last
+                occurrence of it in pattern.
+                The condition s+m < n is necessary for
+                the case when pattern occurs at the end
+                of text */
+                s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
+            }
+
+            else
+                /* Shift the pattern so that the bad character
+                in text aligns with the last occurrence of
+                it in pattern. The max function is used to
+                make sure that we get a positive shift.
+                We may get a negative shift if the last
+                occurrence of bad character in pattern
+                is on the right side of the current
+                character. */
+                s += max(1, j - badchar[txt[s + j]]);
+        }
+    }
+};
 /* Driver code */
 int main()
 {
@@ -101,11 +103,14 @@ int main()
     // Read each line of the file, store
     // it in string s and print it to the
     // standard output stream 
+
+    BoyerMoore bm;
+
     while (getline(file, s))
     {
-        search(s, pat, &count);
+        bm.search(s, pat, &count);
     }
-    
+ 
     cout << "El patrón '" << pat << "' se encontró " << count << " veces en el archivo." << endl;
 
     file.close();
