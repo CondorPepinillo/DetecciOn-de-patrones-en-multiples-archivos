f4d0f00fb0bc2e0e47283c491172c55f81fdc90e
Benja Díaz <benja.diaz.mno10@gmail.com>
2025-07-02 16:26:37 -0400

    Algoritmo KMP agregado

---
+++ kmp.cpp
@@ -0,0 +1,113 @@
+// C++ program to search the pattern in given text using
+// KMP Algorithm
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+void constructLps(string &pat, vector<int> &lps) {
+
+    // len stores the length of longest prefix which
+    // is also a suffix for the previous index
+    int len = 0;
+
+    // lps[0] is always 0
+    lps[0] = 0;
+
+    size_t i = 1;
+    while (i < pat.length()) {
+
+        // If characters match, increment the size of lps
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+
+        // If there is a mismatch
+        else {
+            if (len != 0) {
+
+                // Update len to the previous lps value
+                // to avoid reduntant comparisons
+                len = lps[len - 1];
+            }
+            else {
+
+                // If no matching prefix found, set lps[i] to 0
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+vector<int> search(string &pat, string &txt) {
+    int n = txt.length();
+    int m = pat.length();
+
+    vector<int> lps(m);
+    vector<int> res;
+
+    constructLps(pat, lps);
+
+    // Pointers i and j, for traversing
+    // the text and pattern
+    int i = 0;
+    int j = 0;
+
+    while (i < n) {
+
+        // If characters match, move both pointers forward
+        if (txt[i] == pat[j]) {
+            i++;
+            j++;
+
+            // If the entire pattern is matched
+            // store the start index in result
+            if (j == m) {
+                res.push_back(i - j);
+
+                // Use LPS of previous index to
+                // skip unnecessary comparisons
+                j = lps[j - 1];
+            }
+        }
+
+        // If there is a mismatch
+        else {
+
+            // Use lps value of previous index
+            // to avoid redundant comparisons
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    ifstream file("datasets/English/english_01");
+    if (!file.is_open()) {
+        cerr << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    string pat = "this"; // patrón a buscar
+    string s;
+    int count = 0;
+
+    while (getline(file, s)) {
+        vector<int> positions = search(pat, s);
+        count += positions.size();  // suma cantidad de apariciones
+    }
+
+    cout << "El patrón '" << pat << "' se encontró " << count << " veces en el archivo." << endl;
+    file.close();
+    return 0;
+}
